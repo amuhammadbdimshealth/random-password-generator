a36d5b1122f6395266ee95d0fa21abeee20dfd12
Arif Mohammad Sultan <amuhammadbdimshealth@gmail.com>
2025-12-17 17:29:50 +0600

    Add functionalities: generate random password, password length check, add adebounce to wait for user input, copy to clipboard. Next: Add toggel symbol and number js code.

--- index.js
+++ index.js
@@ -1,6 +1,70 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+""/""];
 
 
+// Store the dom elements in variables
+const generateBtn = document.getElementById("btn-generate-password");
+const passwordOneEl = document.querySelector(".password.one");
+const passwordTwoEl = document.querySelector(".password.two");
+const passwordLengthEl = document.getElementById("password-length");
+let typingTimer;                // Timer identifier
+const doneTypingInterval = 500;  // Time in ms (0.5 seconds)
 
+// Function to generate a random password
+function generatePassword() {
+    let passwordOne = "";
+    let passwordTwo = "";
+    const passwordLength = document.getElementById("password-length").value || 15;
+    for (let i = 0; i < passwordLength; i++) {
+        let randomIndexOne = Math.floor(Math.random() * characters.length);
+        let randomIndexTwo = Math.floor(Math.random() * characters.length);
+        passwordOne += characters[randomIndexOne];
+        passwordTwo += characters[randomIndexTwo];
+    }
+    return { passwordOne, passwordTwo };
+}
+
+// Validate password length input. 
+passwordLengthEl.addEventListener("keyup", () => {
+    clearTimeout(typingTimer); // Why ? Because we want to reset the timer on each keyup event
+    typingTimer = setTimeout(() => {
+        const value = parseInt(passwordLengthEl.value);    
+        if (value < 10) {
+            alert("Password length cannot be less than 10. Setting to minimum value of 10.");
+            passwordLengthEl.value = 10;
+        } else if (value > 20) {
+            passwordLengthEl.value = 20;
+            alert("Password length cannot be more than 20. Setting to maximum value of 20.");
+        }
+    }, doneTypingInterval);
+});
+
+// Copy to clipboard function
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text);
+}
+
+// Event listeners
+// Add click listeners for copying passwords
+passwordOneEl.addEventListener("click", () => {
+    if (passwordOneEl.textContent) {
+        copyToClipboard(passwordOneEl.textContent);
+        alert("Password copied to clipboard!");
+    }
+});
+
+passwordTwoEl.addEventListener("click", () => {
+    if (passwordTwoEl.textContent) {
+        copyToClipboard(passwordTwoEl.textContent);
+        alert("Password copied to clipboard!");
+    }
+});
+
+// Generate passwords on button click
+generateBtn.addEventListener("click", () => {
+    console.log("clicked");
+    const passwordData = generatePassword();
+    passwordOneEl.textContent = passwordData.passwordOne;
+    passwordTwoEl.textContent = passwordData.passwordTwo;
+});
 
